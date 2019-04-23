837331c6b76e4ae012c94a5f1e842a96153143c6
Christian <christian.laun@gmail.com>
2019-04-23 14:05:58 +0200

    Exercise done

---
+++ ex1/ex4.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
+def gaussian(x, h,  mean, std):
+    return h*np.exp(-(x-mean)**2/std**2)
+
+
+samples = np.random.normal(loc=1.0, scale=0.5, size=1000)
+
+print(f'The mean of our sample is {np.mean(samples)}'
+      f', the standard deviation is {np.std(samples)}')
+
+N, bins, patches = plt.hist(samples)
+
+popt, pcov = curve_fit(gaussian, (bins[1:]+bins[:-1])/2, N, p0=[300, 1, 0.5])
+
+x = np.linspace(-1, 4, 100)
+plt.plot(x, gaussian(x, *popt))
+
+plt.show()
+
+print(f'The curve-fitting resulted in a mean of {popt[1]} \n'
+      f'and a standard deviation of {popt[2]} \n'
+      f'The std is higher than the actual std because the binning in \n'
+      f'the histogram resulted in a loss of positional information.')
+
