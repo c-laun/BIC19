0cd0c24fa5de1dffd0b054c453be682e493a6b95
Christian <christian.laun@gmail.com>
2019-04-24 16:21:31 +0200

    added legend and title

--- ex1/ex4.py
+++ ex1/ex4.py
@@ -12,13 +12,15 @@ samples = np.random.normal(loc=1.0, scale=0.5, size=1000)
 print(f'The mean of our sample is {np.mean(samples)}'
       f', the standard deviation is {np.std(samples)}')
 
-N, bins, patches = plt.hist(samples)
+N, bins, patches = plt.hist(samples, label='histogram')
 
 popt, pcov = curve_fit(gaussian, (bins[1:]+bins[:-1])/2, N, p0=[300, 1, 0.5])
 
 x = np.linspace(-1, 4, 100)
-plt.plot(x, gaussian(x, *popt))
+plt.plot(x, gaussian(x, *popt), label='fit')
 
+plt.legend()
+plt.title('Histogram and fitted gaussian')
 plt.show()
 
 print(f'The curve-fitting resulted in a mean of {popt[1]} \n'
